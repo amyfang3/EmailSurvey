d21747087f1825bba417c77e20c4169c785bba71
Amy Fang <amyfang@Amys-MacBook-Pro.local>
2021-07-01 23:26:20 -0500

    initial commmit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// vast majority of projects will only have one express app, but you could have multiple
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// the port that Heroku tells app to listen to, using environment variables (production) or 5000 (development)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
