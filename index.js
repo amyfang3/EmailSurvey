76d5a88afafcc47c7db5154bc42734cb31cb8b51
amyfang3 <afang@uship.com>
2021-09-17 16:55:43 -0500

    Set up Back End to Front End routing in Heroku production environment

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+// if in Heroku production environment
+if (process.env.NODE_ENV === `production`) {
+  // Express will serve up production assets, like main.js or main.css file
+  // looks into the client/build folder for a matching asset
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // the port that Heroku tells app to listen to, using environment variables (production) or 5000 (development)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
